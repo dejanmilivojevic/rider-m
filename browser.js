4587d35b82672e72163698390e9eada7477cb8f8
Dejan Milivojevic <78madman@gmail.com>
2018-12-16 16:31:15 +0100

    Fix

--- browser.js
+++ browser.js
@@ -201,7 +201,7 @@ async function connect() {
       // filters: [{
       //   name: 'Rider-MDB8C'
       //   }],
-      //acceptAllDevices: true, 
+      acceptAllDevices: true, 
       optionalServices: ['0000fee9-0000-1000-8000-00805f9b34fb']
     });
 
